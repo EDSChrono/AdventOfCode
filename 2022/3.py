bf4d22eabf8c8f10738c5b0d246ed743a3c390d2
Ed Smith <ed.smith@rpc-tyche.com>
2022-12-06 10:26:24 +0000

    Day 3 Python

---
+++ 2022/3.py
@@ -0,0 +1,36 @@
+#Define Functions
+def score(c):
+    score_output = ord(c.upper())-ord('A') + 1 + 26 * int('A'<=c<='Z')
+    return score_output
+
+def priority(string_list):
+    result = 0
+    for string in string_list:
+        string_length = len(string)
+        c_1,c_2 = string[:string_length//2], string[string_length//2:]
+        for char in c_1:
+            if char in c_2:
+                result += score(char)
+                break
+    return result
+
+def priority_sum(string_list, group_size):
+    assert len(string_list)%group_size == 0
+    result = 0
+    group_index = 0
+    while group_index < len(string_list): 
+        for char in string_list[group_index]:
+            if char in string_list[group_index+1] and char in string_list[group_index+2]:
+                result += score(char)
+                break
+        group_index += group_size
+    return result
+
+#Import Data
+bag_manifest = [line.strip() for line in open('2022/3.txt')]
+
+#P1
+print(priority(bag_manifest))
+
+#P2
+print(priority_sum(bag_manifest, 3))
