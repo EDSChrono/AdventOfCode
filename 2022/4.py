0057a8661cc0a752607be4fc6b7bab9efee3c1e0
Ed Smith <85060827+EDSChrono@users.noreply.github.com>
2022-12-08 10:59:11 +0000

    Update 4.py
    irregular spacing

--- 2022/4.py
+++ 2022/4.py
@@ -4,8 +4,8 @@ def input_string_to_vector(assign_pair_string):
     endpoint_vector = []
     for line in assign_pair_string:
         assign_list_1,assign_list_2 = line.split(',')
-        l1,u1= assign_list_1.split('-')
-        l2,u2= assign_list_2.split('-')
+        l1,u1 = assign_list_1.split('-')
+        l2,u2 = assign_list_2.split('-')
         endpoint_vector.append([int(x) for x in [l1,u1,l2,u2]])
     return endpoint_vector
 
@@ -14,7 +14,7 @@ def assign_pair_count(assign_pair_endpoints):
     count_nest = 0
     count_dist = 0
     for line in assign_pair_endpoints:
-        l1,u1,l2,u2= line
+        l1,u1,l2,u2 = line
         if not (u1 < l2 or l1 > u2):
             count_dist += 1
         if l1<=l2 and u2<=u1 or l2<=l1 and u1<=u2:
