d9d4a331242a271827054b35a88b5513b55c5568
Ed Smith <ed.smith@rpc-tyche.com>
2022-12-11 16:50:27 +0000

    day 5 python

--- 2022/Python/5.py
+++ 2022/Python/5.py
@@ -1,40 +1,49 @@
-#!/usr/bin/python3
-import sys
-from copy import deepcopy
-infile = sys.argv[1] if len(sys.argv)>1 else '2022/5.txt'
-data = open(infile).read()
-lines = [x for x in data.split('\n')]
-#print(lines)
-S = []
-cmds = []
-for line in lines:
-    if line == '':
-        break
-    sz = (len(line)+1)//4
-    while len(S) < sz:
-        S.append([])
-    for i in range(len(S)):
-        ch = line[1+4*i]
-        if ch != ' ' and 'A'<=ch<='Z':
-            S[i].append(ch)
+# define functions
+def transpose_stack_data(lines):
+    stack_list = []
+    for line in lines:
+        # stop when we reach an empty line
+        if line == '':
+            break
+        no_stacks = (len(line)+1)//4
+        # make empty sublist for stacks
+        while len(stack_list) < no_stacks:
+            stack_list.append([])
+        # populate stack with label if applicable
+        for stack in range(no_stacks):
+            box_label = line[1+4*stack]
+            if 'A'<=box_label<='Z':
+                stack_list[stack].append(box_label)
+    return(stack_list)
 
-print(lines)
-S1 = deepcopy(S)
-S2 = deepcopy(S)
-found = False
-for cmd in lines:
-    if cmd == '':
-        found = True
-        continue
-    if not found:
-        continue
-    words = cmd.split()
-    qty = int(words[1])
-    from_ = int(words[3])-1
-    to_ = int(words[5])-1
-    for (ST, do_rev) in [(S1, True), (S2, False)]:
-        MOVE = ST[from_][:qty]
-        ST[from_] = ST[from_][qty:]
-        ST[to_] = (list(reversed(MOVE)) if do_rev else MOVE) + ST[to_]
-print(''.join([s[0] for s in S1 if len(s)>0]))
-print(''.join([s[0] for s in S2 if len(s)>0]))
+def apply_crate_moves(stack_list, lines, rev = True):
+    found = False
+    for cmd in lines:
+        if cmd == '':
+            found = True 
+            continue
+        if not found:
+            continue
+        words = cmd.split()
+        qty = int(words[1])
+        s_from = int(words[3])-1
+        s_to = int(words[5])-1
+        crates_to_move = stack_list[s_from][:qty]
+        stack_list[s_from] = stack_list[s_from][qty:]
+        stack_list[s_to] = (list(reversed(crates_to_move)) if rev else crates_to_move) + stack_list[s_to]
+    return(stack_list)
+
+# import data
+data = open('2022/5.txt').read()
+lines = [x for x in data.split('\n')]
+
+# transform stacks to list of lists
+stack_list = transpose_stack_data(lines)
+
+# apply transformation (add false argument from p2)
+rearr_stack_list = apply_crate_moves(stack_list, lines, True)
+
+# output code
+print(''.join(rearr_stack_list[i][0] for i in range(len(rearr_stack_list))))
+
+
