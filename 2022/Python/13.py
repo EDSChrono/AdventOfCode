e15f0049fdf8fb99fb637289fa53e844c185a80f
Ed Smith <ed.smith@rpc-tyche.com>
2022-12-13 13:46:23 +0000

    upgrade day 13

--- 2022/Python/13.py
+++ 2022/Python/13.py
@@ -1,5 +1,6 @@
 
-# build comparison function
+# build function
+
 def comp(p1,p2):
     if isinstance(p1, int) and isinstance(p2, int):
         if p1 < p2:
@@ -25,45 +26,59 @@ def comp(p1,p2):
         return comp([p1],p2)
     elif isinstance(p1, list) and isinstance(p2, int):
         return comp(p1,[p2])
-    
+
+def index_sum_sorted_pairs(input):
+    output = 0
+    for index,pair in enumerate(input.split('\n\n')):
+        p1,p2 = pair.split('\n')
+        p1 = eval(p1)
+        p2 = eval(p2)
+        if comp(p1,p2)==-1:
+            output += index+1
+    return output
+
+def list_all_packets(input,dp1,dp2):
+    output = [dp1,dp2]
+    for pair in input.split('\n\n'):
+        p1,p2 = pair.split('\n')
+        p1 = eval(p1)
+        p2 = eval(p2)
+        output.append(p1)
+        output.append(p2)
+    return output
+
+def sort_packets(input):
+    iterate = True
+    while iterate:
+        iter_count = 0
+        for i in range(len(input) - 1):
+            p1 = input[i]
+            p2 = input[i+1]
+            if comp(p1, p2) == 1:
+                input[i] = p2
+                input[i+1] = p1
+                iter_count += 1
+        iterate = iter_count>0
+    return input
+
+def decoder_key(input, p1, p2):
+    output = 1
+    for index,packet in enumerate(input):
+        if packet == p1 or packet == p2:
+            output *= (index+1)
+    return output
+
 
  # import data       
 data = open('2022/13.txt').read().strip()
+dp1,dp2 = [[2]],[[6]]
 
 # calculate part 1
-part1 = 0
-all_packets = [[[2]],[[6]]]
-for index,pair in enumerate(data.split('\n\n')):
-    p1,p2 = pair.split('\n')
-    p1 = eval(p1)
-    p2 = eval(p2)
-    all_packets.append(p1)
-    all_packets.append(p2)
-    if comp(p1,p2)==-1:
-        part1 += index+1
-
+part1 = index_sum_sorted_pairs(data)
 print(part1)
 
 # calculate part 2
-iterate = True
-while iterate:
-    iter_count = 0
-    for i in range(len(all_packets) - 1):
-        p1 = all_packets[i]
-        p2 = all_packets[i+1]
-        if comp(p1, p2) == 1:
-            all_packets[i] = p2
-            all_packets[i+1] = p1
-            iter_count += 1
-    iterate = iter_count>0
-
-part2 = 1
-for index,packet in enumerate(all_packets):
-    if packet == [[2]] or packet == [[6]]:
-        part2 *= (index+1)
-
+all_packets = list_all_packets(data,dp1,dp2)
+sorted_packets = sort_packets(all_packets)
+part2 = decoder_key(sorted_packets,dp1,dp2) 
 print(part2)
-    
-
-
-
